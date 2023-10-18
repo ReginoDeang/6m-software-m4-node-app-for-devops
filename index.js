28d6d64b95afe0229633906c553ff6fe32cd6862
Regino Deang <reginodeangjr@gmail.com>
2023-10-18 19:30:08 +0800

    modified index.js

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-//will get all the onvfig from env file
+// //will get all the onvfig from env file
+// require("dotenv").config();
+
+// const express = require("express");
+
+// //create application
+// const app = express();
+
+// const print = require("./controller");
+
+// //http method and controller method
+// app.get("/", print);
+
+// //will run the app
+// app.listen(process.env.PORT, () => {
+//   console.log(`Listening to port ${process.env.PORT} or default port`);
+// });
+
 require("dotenv").config();
-
 const express = require("express");
-
-//create application
 const app = express();
 
 const print = require("./controller");
 
-//http method and controller method
 app.get("/", print);
+app.get("/hello", (req, res) => {
+  res.send("Hello from the /hello route!");
+});
 
-//will run the app
 app.listen(process.env.PORT, () => {
-  console.log(`Listening to port ${process.env.PORT} or default port`);
+  console.log(`Listening to port ${process.env.PORT} or default port.`);
 });
